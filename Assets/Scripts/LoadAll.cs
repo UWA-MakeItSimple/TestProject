7094b90691ff94227da3a63e7ec8dcd133390dcc
vestlee <vestleet@163.com>
2021-08-27 18:04:17 +0800

    Init Resources Manager

---
+++ Assets/Scripts/LoadAll.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Object o = Resources.Load("Cube");
+        //Instantiate(o);
+        Object obj = null;
+        Instantiate(obj);
+        //AssetBundle go = AssetBundle.LoadFromFile("C:/JJ/ResLoadHook/UnitySample/Assets/AssetBundles/test01");
+        //Instantiate(go.LoadAsset("Cube"));
+        string sp = Application.streamingAssetsPath + "/test/test02";
+        AssetBundle go = AssetBundle.LoadFromFile(sp);
+        Instantiate(go.LoadAsset("Cube") as GameObject);
+
+        //AssetBundleCreateRequest request =  AssetBundle.LoadFromFileAsync("C:/JJ/ResLoadHook/UnitySample/Assets/AssetBundles/myassetbundle");
+        //Instantiate( request.assetBundle.LoadAsset("Cube"));
+        //AssetBundle go = AssetBundle.LoadFromFile("C:/JJ/ResLoadHook/UnitySample/Assets/AssetBundles/myassetbundle");
+        //ResourceRequest go = AssetBundle.LoadFromFileAsync("");
+        //Instantiate(go.LoadAsset("Cube") as GameObject);
+    }
+}
